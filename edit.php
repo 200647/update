4ee591ed96f3d834406b7bc5eff994e55b65bd12
200647 <84138499+200647@users.noreply.github.com>
2021-12-07 20:40:32 +0900

    Create edit.php

--- edit.php
+++ edit.php
@@ -55,12 +55,12 @@
             <div class="col-sm-8 col-sm-offset-2">
                 <div class="form-group">
                     <label for="name"><span class="required">お名前</span> </label>
-                    /* (2)ここに「名前」欄の初期表示処理を追加して下さい  */
+              
                     <input type="text" id="name" name="name" value="<?php echo $row[name];?>" required>
                 </div>
                 <div class="form-group">
                     <label for="email"><span class="required">メールアドレス</span> </label>
-                    /* (3)ここに「メールアドレス」欄の初期表示処理を追加して下さい  */
+                   
                     <input type="email" id="email" name="email" value="<?php echo $row[email];?>" required> 
 
                 </div>
@@ -71,7 +71,7 @@
                           echo "<label class='radio-inline'>";
                           echo "    <input type='radio' name='gender' value='1' required";
                           if( $row[gender] === 1 ) {
-　　　　　　　　　　　　　　　　/* (4)ここにラジオボタンの初期表示処理を追加してください  */
+　　　　　　　　　　　
                              echo " checked>男性";
                           } else {
                           	 echo ">男性";
@@ -80,7 +80,7 @@
                           echo "<label class='radio-inline'>";
                           echo "    <input type='radio' name='gender' value='2' required";
                           if( $row[gender] === 2 ) {
-　　　　　　　　　　　　　　　　/* (5)ここにラジオボタンの初期表示処理を追加してください  */
+　　　　　　　
                              echo " checked>女性";
                           } else {
                           	 echo ">女性";
@@ -89,7 +89,7 @@
                           echo "<label class='radio-inline'>";
                           echo "    <input type='radio' name='gender' value='9' required";
                           if( $row[gender] === 9 ) {
-　　　　　　　　　　　　　　　　/* (6)ここにラジオボタンの初期表示処理を追加してください  */
+　　　　　　　　　　　　　　
                              echo " checked>その他";
                           } else {
                           	 echo ">その他";
